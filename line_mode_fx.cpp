c02c4966786b5c210f8a3f4f745abd8281890e1d
hades <328705509@qq.com>
2017-07-20 19:27:27 +0800

    Update line_mode_fx.cpp

--- line_mode_fx.cpp
+++ line_mode_fx.cpp
@@ -16,14 +16,14 @@ typedef long long LL;
 using namespace std;
 const LL MOD = 1e9+7;
 const int N = 100005;
-int n,p;
-int pa[2*N],pb[2*N],pc[2*N];
-int edge[N],point[2*N],next[2*N],back[2*N],ans[N];
+int n;
+LL pa[2*N],pb[2*N],pc[2*N],cir,ans[N],p;
+int edge[N],point[2*N],nextc[2*N],back[2*N];
 bool has_p[N],has_e[2*N];
 map<LL,LL> rec;
 
 LL gcd(LL a,LL b){return b==0?a:gcd(b,a%b);}
-LL extend_gcd(LL a,LL b,LL &x,LL &y)//扩展欧几里得
+LL extend_gcd(LL a,LL b,LL &x,LL &y)
 {
     if(b==0){x=1;y=0;return a;}
     else {
@@ -43,23 +43,21 @@ LL inv(LL a, LL m)
 class graph_adlist
 {
 private:
-    vector<int> edge,point,next,back;
-    vector<bool> has_p,has_e;
     int n,m,e;
 public:
     graph_adlist(int n1,int m1)
     {
-        n=n1;m=m1;
+        n=n1;m=m1; e=0;
         mem(edge);
-        mem(next);
+        mem(nextc);
         mem(has_p);
         mem(has_e);
         mem(back);
     }
-    void Link2(int a,int b,int x,int y,int z)
+    void Link2(int a,int b,LL x,LL y,LL z)
     {
-        point[++e]=b; pa[e]=x; pb[e]=y; pc[e]=z; next[e]=edge[a]; edge[a]=e;
-        point[++e]=a; pa[e]=x; pb[e]=y; pc[e]=z; next[e]=edge[b]; edge[b]=e;
+        point[++e]=b; pa[e]=x; pb[e]=y; pc[e]=z; nextc[e]=edge[a]; edge[a]=e;
+        point[++e]=a; pa[e]=y; pb[e]=x; pc[e]=z; nextc[e]=edge[b]; edge[b]=e;
         back[e]=e-1; back[e-1]=e;
     }
     void read()
@@ -76,7 +74,7 @@ public:
     {
         int j,k;
         has_p[i]=1;
-        for(j=edge[i];j;j=next[j])
+        for(j=edge[i];j;j=nextc[j])
         {
             if(has_e[j]) continue;
             has_e[j]=has_e[back[j]]=1;
@@ -94,25 +92,31 @@ public:
     }
     bool GetOneAns(int i,LL a,LL b,LL c)
     {
+        //printf("i=%d\n",i);
+        //printf("a=%lld b=%lld c=%lld\n",a,b,c);
+
         int j,k;
         has_p[i]=1;
-        for(j=edge[i];j;j=next[j])
+        for(j=edge[i];j;j=nextc[j])
         {
             if(has_e[j]) continue;
             has_e[j]=has_e[back[j]]=1;
             k=point[j];
+            //printf("GOAk:%d\n",k);
             //option begin
             if (k==cir)
             {
-                ans[cir]=(pc[j]-pa[j]*inv(b,p)%p*c1%p+p)%p * inv((pb[j]-pa[j]*inv(b,p)%p+p)%p,p)
+                ans[cir]=(pc[j]-c*pa[j]%p*inv(b,p)%p%p+p)%p * inv((pb[j]-a*pa[j]%p*inv(b,p)%p+p)%p,p)%p;
                 return 1;
             }
             //option end
-            if(a==0&&n==0&&c==0)
+            if(a+b+c==0)
+            {
                 if (GetOneAns(k,pa[j],pb[j],pc[j]))
                     return 1;
+            }
             else
-                if (GetOneAns(k, pa[j]*inv(b,p)%p , pb[j], (pc[j]-pa[j]*inv(b,p)%p*c1%p+p)%p ))
+                if (GetOneAns(k, (p-a*pa[j]%p*inv(b,p)%p)%p , pb[j], (pc[j]-c*pa[j]%p*inv(b,p)%p%p+p)%p ))
                     return 1;
         }
         return 0;
@@ -121,13 +125,17 @@ public:
     {
         int j,k;
         has_p[i]=1;
-        for(j=edge[i];j;j=next[j])
+        for(j=edge[i];j;j=nextc[j])
         {
             if(has_e[j]) continue;
             has_e[j]=has_e[back[j]]=1;
             k=point[j];
             //option begin
-            ans[k]=(pc[j])
+            //pa[j]x1+pb[j]x2=pc[j]
+            if(k!=cir)
+                ans[k]=((pc[j]-pa[j]*x%p+p)%p)*inv(pb[j],p)%p;
+            if(!has_p[k])
+                GetAllAns(k,ans[k]);
             //option end
         }
         return 0;
@@ -138,11 +146,17 @@ public:
         int i,u,v,c;
         read();
         cir = Traversal(1);
-
+        //printf("cir=%d\n",cir);
         mem(has_e);
         mem(has_p);
         GetOneAns(cir,0,0,0);
+        //printf("ans[cir]=%d\n",ans[cir]);
+        mem(has_e);
+        mem(has_p);
+        GetAllAns(cir,ans[cir]);
 
+        for(i=1;i<=n;i++)
+            printf("%lld\n",ans[i]);
     }
 };
 
@@ -153,7 +167,8 @@ void init()
 
 void work()
 {
-
+    graph_adlist g(n,2*n);
+    g.Solution();
 }
 
 void prep()
