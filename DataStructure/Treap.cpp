190d251c47cc8a80749faa3f2d0ab9713460a049
hades <328705509@qq.com>
2017-08-31 01:02:22 +0800

    Update and rename Treap(fixing).cpp to Treap.cpp

--- DataStructure/Treap.cpp
+++ DataStructure/Treap.cpp
@@ -24,8 +24,8 @@ struct node
 {
     node *father,*ch[2];
     int key,weight;
-    node() {}
-    node(int x,node *fa = NULL):key(x),father(fa)
+    inline node() {}
+    inline node(int x,node *fa = NULL):key(x),father(fa)
     {
         ch[0] = ch[1] = NULL;
         weight = rand();
@@ -34,79 +34,46 @@ struct node
 
 struct Treap
 {
-    node *T[N];
+    node *T[N],*root;
     int tot;
-    Treap()
-    {
-        tot=0;
-        mem(T);
-    }
-    void Path(node* rt)
-    {
-        if(rt == NULL ) return;
-        printf("weight=%d x=%d:l=%d r=%d\n",rt->weight,rt->key,rt->ch[0]?rt->ch[0]->key:0,rt->ch[1]?rt->ch[1]->key:0);
-        Path(rt->ch[0]);
-        Path(rt->ch[1]);
-    }
-    inline bool lr(node* f,node* s)
-    {
-        return f->ch[1]==s;
-    }
+    Treap(){tot=0;mem(T);}
+    inline bool lr(node* f,node* s){return f->ch[1]==s;}
     inline void lrr(node *a,int lrl)
     {
-        node *b = a->ch[lrl^1];
-        node *f = a->father;
-        node *d = b->ch[lrl];
-        b->father = f;
-        if(f) f->ch[lr(f,a)] = b;
-        a->ch[lrl^1] = d;
-        if(d) d->father = a;
-        b->ch[lrl] = a;
-
-        a->father = b;
+        node *b = a->ch[lrl^1],*f = a->father,*d = b->ch[lrl];
+        b->father = f; if(f) f->ch[lr(f,a)] = b; else root = b;
+        a->ch[lrl^1] = d; if(d) d->father = a;
+        b->ch[lrl] = a; a->father = b;
     }
     node* insert(node*rt,int x,node* fa,int lr)
     {
         if(rt==NULL)
         {
             T[tot] = new node(x,fa);
+            if (tot==0) root=T[0];
             if(fa) fa->ch[lr] = T[tot];
             return T[tot++];
         }
         else
-        {
             return insert(rt->ch[x > rt->key], x, rt, x > rt->key);
-        }
     }
-    void rotate(int x)
+    inline void rotate(int x)
     {
-        node *rt=insert(T[0],x,NULL,0);
+        node *rt=insert(root,x,NULL,0);
         for(node*fa=rt->father; fa!=NULL &&rt->weight < fa->weight; fa = rt->father)
-        {
-            Path(T[0]);
-            printf("\n");
             lrr(fa,lr(fa,rt)^1);
-        }
-        printf("\n");
-        Path(T[0]);
-        printf("\n");
     }
     int query(int x)
     {
-        node *rt=T[0];
+        node *rt=root;
         int ans = -INF;
-        printf("query %d:",x);
         while(rt!=NULL)
-        {
-            printf("%d ",rt->key);
             if(rt->key<=x &&rt->key > ans)
             {
                 ans = rt->key;
                 rt = rt->ch[1];
             }
             else rt = rt->ch[0];
-        }
-        printf("\n");
         return ans;
     }
 } *treap;
@@ -141,7 +108,6 @@ int main()
     {
         init();
         work();
-        printf("\n\n");
     }
     return 0;
 }
